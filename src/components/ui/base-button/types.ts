a19dab5256cfb16810769225c7a42c47f8394de8
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-16 04:51:33 +0500

    fix: поправил стили в кнопке и пропсы (добавил пропс type)

--- src/components/ui/base-button/types.ts
+++ src/components/ui/base-button/types.ts
@@ -3,5 +3,6 @@ type BtnType = "primary" | "secondary" | "danger";
 export interface IBaseButtonProps {
   labelText?: string;
   btnType?: BtnType;
+  type?: "button" | "submit" | "reset";
   loading?: boolean;
 }
