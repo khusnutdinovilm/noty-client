703e8d632b147b4ceb43ca7292a67091bf3f7dd0
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-15 19:41:06 +0500

    chore: clear and config project
    - удалил ненужные компоненты;
    - прописал alias'ы для файлов;
    - прописал правила для eslint.config.ts;
    - применил данные правила для всех файлов (команда npm run lint);
    - установил sass;
    - настроил работу с sass-стилями.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
 
-import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,7 +18,29 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      assets: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      styles: fileURLToPath(new URL("./src/styles", import.meta.url)),
+      icons: fileURLToPath(new URL("./src/icons", import.meta.url)),
+      layouts: fileURLToPath(new URL("./src/layouts", import.meta.url)),
+      ui: fileURLToPath(new URL("./src/components/ui", import.meta.url)),
+      shared: fileURLToPath(new URL("./src/components/shared", import.meta.url)),
+      modules: fileURLToPath(new URL("./src/modules", import.meta.url)),
+      api: fileURLToPath(new URL("./src/api", import.meta.url)),
+      router: fileURLToPath(new URL("./src/router", import.meta.url)),
+      pages: fileURLToPath(new URL("./src/pages", import.meta.url)),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+        additionalData: `
+          @import "styles/variables/index.scss";
+          @import "styles/functions/index.scss";
+          @import "styles/mixins/index.scss";
+        `,
+      },
     },
   },
 });
