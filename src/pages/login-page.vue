6d56a33a7222b49bf3eec1400f0af5c82b38597e
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-16 04:38:22 +0500

    feat: переверстал страницу логина и авторизационный лэйаут

--- src/pages/login-page.vue
+++ src/pages/login-page.vue
@@ -1,8 +1,17 @@
 <template>
-  <main>
-    <h1>This is a login page</h1>
-    <router-link to="/">home</router-link>
-  </main>
+  <auth-page-template page-title="Добро пожаловать" page-subtitle="Введите ваш логин и пароль">
+    <template #auth-page-template-icon>
+      <lock-icon />
+    </template>
+  </auth-page-template>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LockIcon from "icons/lock-icon.vue";
+
+import AuthPageTemplate from "shared/auth-page-template";
+
+defineOptions({
+  name: "login-page",
+});
+</script>
