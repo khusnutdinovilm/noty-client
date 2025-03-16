41bc901f97508c1536ba9b1ac28ff8689ad514f8
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-16 05:03:17 +0500

    feat: добавил форму авторизации на странице логина; сделал переход на домашнюю страницу после успешной авторизации; добавил стили к странице

--- src/pages/login-page.vue
+++ src/pages/login-page.vue
@@ -1,17 +1,44 @@
 <template>
-  <auth-page-template page-title="Добро пожаловать" page-subtitle="Введите ваш логин и пароль">
+  <auth-page-template
+    page-title="Добро пожаловать"
+    page-subtitle="Введите ваш логин и пароль"
+    class="login-page"
+  >
     <template #auth-page-template-icon>
       <lock-icon />
+    </template>
+
+    <template #auth-page-template-body>
+      <login-form @success-login="onSuccessLogin" />
     </template>
   </auth-page-template>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
 import LockIcon from "icons/lock-icon.vue";
 
 import AuthPageTemplate from "shared/auth-page-template";
 
+import LoginForm from "modules/auth/components/forms/login-form.vue";
+
 defineOptions({
   name: "login-page",
 });
+
+const router = useRouter();
+
+const onSuccessLogin = (accessToken: string) => {
+  localStorage.setItem("access-token", accessToken);
+  router.replace({ name: "home-page" });
+};
 </script>
+
+<style lang="scss">
+.login-page {
+  & .auth-page-template__body {
+    width: 100%;
+  }
+}
+</style>
