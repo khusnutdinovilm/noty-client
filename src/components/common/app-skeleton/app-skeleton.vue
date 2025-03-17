527998c524fcbcb0a98964a6c0077e0e2841294a
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-17 18:00:43 +0500

    feat: разработал компонент скелетона

---
+++ src/components/common/app-skeleton/app-skeleton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="appSkeletonRef" class="app-skeleton"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+defineOptions({
+  name: "app-skeleton",
+});
+
+const props = defineProps<{
+  width?: string;
+  height?: string;
+  type?: "circle" | "text";
+}>();
+
+const appSkeletonRef = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  if (appSkeletonRef.value) {
+    if (props.width) {
+      console.log("kek");
+      appSkeletonRef.value.style.setProperty("--skeleton-width", props.width);
+    }
+
+    if (props.height) {
+      appSkeletonRef.value.style.setProperty("--skeleton-height", props.height);
+    }
+
+    if (props.type === "circle") {
+      appSkeletonRef.value.style.setProperty("--skeleton-border-radius", "50%");
+    }
+  }
+});
+</script>
