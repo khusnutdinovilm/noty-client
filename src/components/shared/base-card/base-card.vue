ce6b486782971244827e7c5ff8734f6999f59c11
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-17 15:25:59 +0500

    feat: разработал переиспользуемый компонент карточки

---
+++ src/components/shared/base-card/base-card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="base-card">
+    <div class="base-card__content">
+      <div class="base-card__title">
+        {{ title }}
+      </div>
+
+      <div class="base-card__description">
+        {{ description }}
+      </div>
+    </div>
+
+    <div class="base-card__footer">
+      <slot name="base-card-footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "base-card",
+});
+
+defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
