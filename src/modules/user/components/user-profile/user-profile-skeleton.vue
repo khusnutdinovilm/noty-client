8f26548701a7e1f510561fce01799cd4b445179c
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-03-17 18:05:24 +0500

    feat: разработал компонент user-profile-skeleton, который будет отображаться, пока информация о пользователе загружается

---
+++ src/modules/user/components/user-profile/user-profile-skeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-profile user-profile--skeleton">
+    <app-skeleton type="circle" width="48px" height="48px" />
+
+    <div class="user-profile__user-info">
+      <app-skeleton width="100px" height="16px" />
+
+      <app-skeleton width="150px" height="16px" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppSkeleton from "common/app-skeleton";
+
+defineOptions({
+  name: "user-profile-skeleton",
+});
+</script>
